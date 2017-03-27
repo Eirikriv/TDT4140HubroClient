fcac9503ef60b57d90b00e8bfb8dbfe108b10ff0
johannzv <joahnnes.z.vollan@gmail.com>
2017-03-27 11:41:30 +0200

    linting

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import './index.css';
 import {ApolloProvider} from 'react-apollo';
 import ApolloClient, {createNetworkInterface} from 'apollo-client';
 
-const networkInterface = createNetworkInterface({ uri: 'http://localhost:5000/graphql' });
-const client = new ApolloClient({
+const networkInterface= createNetworkInterface({ uri: 'http://localhost:5000/graphql' });
+const client= new ApolloClient({
   networkInterface,
 });
 
 ReactDOM.render(
-    <ApolloProvider client ={client}>
+    <ApolloProvider client={client}>
   <App/>
   </ApolloProvider>,document.getElementById('root'));
