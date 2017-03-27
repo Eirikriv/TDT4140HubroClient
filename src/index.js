a4bca6efbd507059fb9570fbc36b2530a55995da
johannzv <joahnnes.z.vollan@gmail.com>
2017-03-27 11:29:43 +0200

    fixed build problem

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import './index.css';
 
 
@@ -15,7 +15,5 @@ const client = new ApolloClient({
 
 ReactDOM.render(
     <ApolloProvider client ={client}>
-  <App />
-  ,
-  document.getElementById('root')
-);
+  <App/>
+  </ApolloProvider>,document.getElementById('root'));
