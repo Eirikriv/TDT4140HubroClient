3f35c6500d8abc148fa5ffca6869e2d774efe856
johannzv <joahnnes.z.vollan@gmail.com>
2017-04-12 13:38:18 +0200

    started time-pick avg

--- src/components/Settings/avgTimePerAssignmentCourse.js
+++ src/components/Settings/avgTimePerAssignmentCourse.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import _ from 'lodash'
+import TimePicker from 'material-ui/TimePicker'
 
 class AvgTimePerAssignmentCourse extends React.Component{
   constructor(props){
@@ -7,16 +9,28 @@ class AvgTimePerAssignmentCourse extends React.Component{
     this.renderList = this.renderList.bind(this)
     this.state = {coursesSelected:props.coursesSelected}
     this.handleChange = this.handleChange.bind(this)
+    this.convertDate = this.convertDate.bind(this)
   }
 componentWillReceiveProps(nextProps){
 //console.log(nextProps);
 this.setState({coursesSelected:nextProps.coursesSelected})
 }
 
-handleChange(event){
+convertDate(string){
+  let convertedDate = new Date();
+
+string = _.split(string, ':', 3)
+  let hours = parseInt(string[0],10)
+  let minutes = parseInt(string[1],10)
+  convertedDate.setHours(hours)
+  convertedDate.setMinutes(minutes)
+  return convertedDate
+}
+handleChange(event, date){
   // let avgTime = event.target.value
   // let courseId = event.target.id
   // let courseName = event.target.name
+  console.log(date);
 }
 renderList(){
 
@@ -29,8 +43,15 @@ renderList(){
             {course.courseName}
           </td>
           <td>
+            <TimePicker
+                      format="24hr"
+                      hintText="Start time"
+                      name={course.courseName}
+                      id={course.courseId}
+                      value={this.convertDate(course.avgAssignmentTime)}
+                      onChange={this.handleChange}
+                    />
 
-            <input type="text" id={course.courseId} name={course.courseName} value={course.avgAssignmentTime} onChange={this.handleChange}/>
           </td>
 
         </tr>
@@ -38,6 +59,7 @@ renderList(){
     })
   )
 }
+//<input type="text" id={course.courseId} name={course.courseName} value={course.avgAssignmentTime} onChange={this.handleChange}/>
 
   render(){
     return (<div>
