0b370f4b3d345c06874f06207af3985ee77b63ec
johannzv <joahnnes.z.vollan@gmail.com>
2017-04-12 23:33:47 +0200

    deleted unused files

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import Outer from './outerComponent'
-import Navbar from './Navbar2/index'
+import Navbar from './Navbar/index'
 class App extends Component {
   render() {
     return (
--- src/components/Navbar2/homeIcon.js
+++
@@ -1,9 +0,0 @@
-import hubroLogo from '../../assets/images/hubroLogo.svg'
-import styled from 'styled-components'
-
-export default styled.div`
-background-image: url(${hubroLogo});
-background-repeat: no-repeat;
-width: 50px;
-height:50px;
-`
