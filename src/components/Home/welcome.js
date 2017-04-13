4fadb4755c79810c4c41014f4383fd18708c8723
johannzv <joahnnes.z.vollan@gmail.com>
2017-04-13 18:35:59 +0200

    welcome

--- src/components/Home/welcome.js
+++ src/components/Home/welcome.js
@@ -1,4 +1,13 @@
 import React from 'react'
 export default (props)=>{
-  return(<div>welcome</div>)
+  return(
+    <div className="section-blocks">
+      <div className="welcome-right">
+      <h1 className="section-title">Welcome to Hubro!</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div className="welcome-left">
+      <div className="big-hubro-logo"></div>
+    </div>
+    </div>)
 }
