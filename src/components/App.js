e43874e81b041ea9dbc012fb06364f8df4316ec9
johannzv <joahnnes.z.vollan@gmail.com>
2017-04-14 01:22:51 +0200

    the team

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@
 import React, { Component } from 'react';
 import Outer from './outerComponent'
 import Navbar from './Navbar/index'
+import Footer from './Footer/index'
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Navbar/>
         {this.props.children}
-	</div>
+        <Footer/>
+	     </div>
     )
   }
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default  (props)=>{
+  return(
+    <footer>
+      som some
+    </footer>
+  )
+}
