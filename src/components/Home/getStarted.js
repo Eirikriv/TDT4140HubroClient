3497669a0af8d28b42c746b87bf551fd39e5b8cd
Eirikriv <eirik.rivedal@gmail.com>
2017-04-22 14:32:47 +0200

    Fixed loginwithgoogle button

--- src/components/Home/getStarted.js
+++ src/components/Home/getStarted.js
@@ -10,6 +10,8 @@ export default (props)=>{
           and time prefernces.
         </p>
       </article>
+      <a href = "https://accounts.google.com/o/oauth2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fplus.me%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&response_type=code&client_id=82575189460-qsqfcpmenbna11tdup9rval3oi78trlj.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Fapi.v1.hubro.tech%2Fauth%2Foauth2callback">
       <img className="loginGoogle" src={require('../../assets/images/sign-in-with-google.png')} alt="login"/>
+      </a>
   </section>)
 }
