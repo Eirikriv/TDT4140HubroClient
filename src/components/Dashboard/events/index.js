cf5006109b94707a88f6b3bd9795c95ccf07383a
Eirikriv <eirik.rivedal@gmail.com>
2017-04-22 14:47:17 +0200

    Cosmetic changes

--- src/components/Dashboard/events/index.js
+++ src/components/Dashboard/events/index.js
@@ -20,10 +20,10 @@ class InsertNewEvents extends React.Component{
         <section className="section-block" id="setting-event">
           <h1>Welcome, <span>{this.props.data.user.studentName}</span> </h1>
           <article>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Now that you are logged in, we suggest that you choose which courses you want Hubro to add to your google calendar. After selecting, hit the button to add events to your calendar</p>
           </article>
           <h6>Last updatadet: {this.props.data.user.updatedCalendars}</h6>
-          <button onClick={this.onClickHandler}> Klikk på meg</button>
+          <button onClick={this.onClickHandler}> Add events</button>
         </section>
       )
     }
