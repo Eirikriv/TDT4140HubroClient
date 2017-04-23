57463f6d60fe740959fd70a275a72d1cd7a75b77
Helene <helene.h.saether@gmail.com>
2017-04-23 03:26:21 +0200

    centered and changed size of images, removed italic font  how it works

--- src/components/Home/howItWorks.js
+++ src/components/Home/howItWorks.js
@@ -4,42 +4,53 @@ export default (props)=>{
         <h1>How it works</h1>
         <div>
 <table>
-<tbody>
-  <tr>
-  <td>
+    <tbody>
+    <tr>
+        <td>
+            <figure className="how-it-works-figure1">
+                <figcaption>Step 1: connect to google calendar</figcaption>
+                <div>
+                    <img className="googleLogo" src={require('../../assets/images/googleIcon.png')} alt=""/>
+                </div>
 
-      <figure className="how-it-works-figure1">
-          <figcaption>Step 1: connect to google calendar</figcaption>
-          <img src={require('../../assets/images/googleIcon.jpg')} alt=""/>
-          </figure>
+            </figure>
         </td>
         <td>
 
-          <figure>
-          <figcaption>Step 2: scrape relevant platforms</figcaption>
-          <img src={require('../../assets/images/itslearning_logo')} alt=""/>
-      </figure>
-    </td>
-      </tr>
-<tr>
-<td>
+            <figure>
+                <figcaption>Step 2: scrape relevant platforms</figcaption>
+                <div>
+                    <img className="itsLogo" src={require('../../assets/images/itslearning_logo.png')} alt=""/>
+                </div>
 
-        <figure className="how-it-works-figure3">
-          <figcaption>Step 3: compute optimal solutions for you</figcaption>
-          <img src={require('../../assets/images/brain-icon-png-15.png')} alt=""/>
-        </figure>
-      </td>
-      <td>
+            </figure>
+        </td>
+    </tr>
+    <tr>
+        <td>
 
-        <figure className="how-it-works-figure4">
-          <figcaption>Step 4: insert events into your calendar</figcaption>
-          <img src={require('../../assets/images/calendarGoogle.jpg')} alt=""/>
+            <figure className="how-it-works-figure3">
+                <figcaption>Step 3: compute optimal solutions for you</figcaption>
+                <div>
+                    <img className="brainLogo" src={require('../../assets/images/brain-icon-png-15.png')} alt=""/>
+                </div>
 
-        </figure>
-      </td>
-      </tr>
-      </tbody>
-      </table>
-      </div>
-        </section>)
+            </figure>
+        </td>
+        <td>
+
+            <figure className="how-it-works-figure4">
+                <figcaption>Step 4: insert events into your calendar</figcaption>
+                <div>
+                    <img className="calendarLogo" src={require('../../assets/images/calendarGoogle.jpg')} alt=""/>
+                </div>
+
+
+            </figure>
+        </td>
+    </tr>
+    </tbody>
+</table>
+        </div>
+  </section>)
 }
